45e875ece0b24bb9629f488d7b7dc724b883e773
hrtk91 <ht91218@gmail.com>
2024-03-03 12:37:10 +0900

    fix; AngleLeftのdefaultのopacity設定を削除

--- lean-scala.client/src/app/components/UI/Icons/AngleLeft.tsx
+++ lean-scala.client/src/app/components/UI/Icons/AngleLeft.tsx
@@ -7,7 +7,6 @@ export const AngleLeft = (props: SVGProps<SVGSVGElement>) => {
         d="m9.243 12.781-4.25-4.25a.748.748 0 0 1 .003-1.062l4.25-4.25a.747.747 0 0 1 1.06 0l.703.712a.747.747 0 0 1 0 1.06L7.996 8.003l3.013 3.013a.747.747 0 0 1 0 1.059l-.706.706a.747.747 0 0 1-1.06 0z"
         fill="#212423"
         fillRule="nonzero"
-        opacity=".3"
       />
     </svg>
   );
